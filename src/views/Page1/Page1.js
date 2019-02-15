8be43ad875e3e7a835f46917b8d81c1258a8b0af
jensen5201 <jensen5201@163.com>
2019-02-15 14:52:28 +0800

    [add] prettier

--- src/views/Page1/Page1.js
+++ src/views/Page1/Page1.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import './Page1.css';
-import image from '@/assets/imgs/004.jpg'
+import React, { Component } from "react";
+import "./Page1.css";
+import image from "@/assets/imgs/004.jpg";
 
 export default class Home extends Component {
-  render  () {
+  render() {
     return (
       <div className="page-box">
         this is page1~
         <img src={image} />
       </div>
-    )
+    );
   }
 }
