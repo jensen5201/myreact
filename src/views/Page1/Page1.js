5306df371ff98b6471076e0c7a5fe96bc9d9a152
jensen5201 <jensen5201@163.com>
2019-02-15 15:07:12 +0800

    test

--- src/views/Page1/Page1.js
+++ src/views/Page1/Page1.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import "./Page1.css";
-import image from "@/assets/imgs/004.jpg";
+import React, { Component } from 'react'
+import './Page1.css'
+import image from '@/assets/imgs/004.jpg'
 
 export default class Home extends Component {
   render() {
@@ -9,6 +9,6 @@ export default class Home extends Component {
         this is page1~
         <img src={image} />
       </div>
-    );
+    )
   }
 }
